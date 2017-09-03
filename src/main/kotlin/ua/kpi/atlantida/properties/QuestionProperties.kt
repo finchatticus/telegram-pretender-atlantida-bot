9592acae1f63479d2df4e30c4c8b18ebcbbf89c4
vlad <kozii.vladyslav@gmail.com>
2017-09-04 00:28:30 +0300

    Update questions

--- src/main/kotlin/ua/kpi/atlantida/properties/QuestionProperties.kt
+++ src/main/kotlin/ua/kpi/atlantida/properties/QuestionProperties.kt
@@ -4,17 +4,15 @@ class QuestionProperties : MyProperties(FILE_NAME) {
 
     private companion object {
         private val FILE_NAME = "questions/ua.properties"
-        private val PROP_SURNAME = "surname"
         private val PROP_NAME = "name"
-        private val PROP_PATRONYM = "patronym"
         private val PROP_LEVEL = "level"
         private val PROP_FACULTY = "faculty"
         private val PROP_SWIMMING = "swimming"
         private val PROP_SWIMMING_LEVEL = "swimming_level"
+        private val PROP_SWIMMING_RATING = "swimming_rating"
         private val PROP_PHONE = "phone"
         private val PROP_EMAIL = "email"
         private val PROP_PROFILE = "profile"
-        private val PROP_HOBBIES = "hobbies"
         private val PROP_MOTIVATION = "motivation"
         private val PROP_MARKETING = "marketing"
         private val PROP_LEVELS = "levels"
@@ -23,17 +21,15 @@ class QuestionProperties : MyProperties(FILE_NAME) {
         private val PROP_NO = "no"
     }
 
-    val surname = readProperties(PROP_SURNAME)
     val name = readProperties(PROP_NAME)
-    val patronym = readProperties(PROP_PATRONYM)
     val level = readProperties(PROP_LEVEL)
     val faculty = readProperties(PROP_FACULTY)
     val swimming = readProperties(PROP_SWIMMING)
     val swimmingLevel = readProperties(PROP_SWIMMING_LEVEL)
+    val swimmingRating = readProperties(PROP_SWIMMING_RATING)
     val phone = readProperties(PROP_PHONE)
     val email = readProperties(PROP_EMAIL)
     val profile = readProperties(PROP_PROFILE)
-    val hobbies = readProperties(PROP_HOBBIES)
     val motivation = readProperties(PROP_MOTIVATION)
     val marketing = readProperties(PROP_MARKETING)
     val levels = readProperties(PROP_LEVELS).split(",")
