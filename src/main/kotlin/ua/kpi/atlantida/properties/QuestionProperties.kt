81d15d46df7225980d6f70c6ef729958cc53fbcf
vlad <kozii.vladyslav@gmail.com>
2017-09-06 00:39:56 +0300

    Add error messages to question properties

--- src/main/kotlin/ua/kpi/atlantida/properties/QuestionProperties.kt
+++ src/main/kotlin/ua/kpi/atlantida/properties/QuestionProperties.kt
@@ -6,12 +6,18 @@ class QuestionProperties : MyProperties(FILE_NAME) {
         private val FILE_NAME = "questions/ua.properties"
         private val PROP_NAME = "name"
         private val PROP_LEVEL = "level"
+        private val PROP_LEVEL_ERROR = "level_error"
         private val PROP_FACULTY = "faculty"
+        private val PROP_FACULTY_ERROR = "faculty_error"
         private val PROP_SWIMMING = "swimming"
+        private val PROP_SWIMMING_ERROR = "swimming_error"
         private val PROP_SWIMMING_LEVEL = "swimming_level"
+        private val PROP_SWIMMING_LEVEL_ERROR = "swimming_level_error"
         private val PROP_SWIMMING_RATING = "swimming_rating"
         private val PROP_PHONE = "phone"
+        private val PROP_PHONE_ERROR = "phone_error"
         private val PROP_EMAIL = "email"
+        private val PROP_EMAIL_ERROR = "email_error"
         private val PROP_PROFILE = "profile"
         private val PROP_MOTIVATION = "motivation"
         private val PROP_MARKETING = "marketing"
@@ -24,12 +30,18 @@ class QuestionProperties : MyProperties(FILE_NAME) {
 
     val name = readProperties(PROP_NAME)
     val level = readProperties(PROP_LEVEL)
+    val levelError = readProperties(PROP_LEVEL_ERROR)
     val faculty = readProperties(PROP_FACULTY)
+    val facultyError = readProperties(PROP_FACULTY_ERROR)
     val swimming = readProperties(PROP_SWIMMING)
+    val swimmingError = readProperties(PROP_SWIMMING_ERROR)
     val swimmingLevel = readProperties(PROP_SWIMMING_LEVEL)
+    val swimmingLevelError = readProperties(PROP_SWIMMING_LEVEL_ERROR)
     val swimmingRating = readProperties(PROP_SWIMMING_RATING)
     val phone = readProperties(PROP_PHONE)
+    val phoneError = readProperties(PROP_PHONE_ERROR)
     val email = readProperties(PROP_EMAIL)
+    val emailError = readProperties(PROP_EMAIL_ERROR)
     val profile = readProperties(PROP_PROFILE)
     val motivation = readProperties(PROP_MOTIVATION)
     val marketing = readProperties(PROP_MARKETING)
