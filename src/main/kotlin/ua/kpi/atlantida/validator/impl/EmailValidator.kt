34908df36128d299222413f1231f6b2ab92fb525
vlad <kozii.vladyslav@gmail.com>
2017-09-08 23:11:44 +0300

    Fix email regex

--- src/main/kotlin/ua/kpi/atlantida/validator/impl/EmailValidator.kt
+++ src/main/kotlin/ua/kpi/atlantida/validator/impl/EmailValidator.kt
@@ -5,7 +5,7 @@ import ua.kpi.atlantida.validator.Validator
 class EmailValidator(private val error: String) : Validator<String> {
 
     private companion object {
-        const val EMAIL_REGEX = "(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+        const val EMAIL_REGEX = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
     }
 
     override fun isValid(value: String) = value.matches(Regex(EMAIL_REGEX))
