ce75cec107a1a20dc77f62b00e77bb84a2f9451d
vlad <kozii.vladyslav@gmail.com>
2018-09-05 13:40:01 +0300

    Refactoring properties

--- src/main/kotlin/ua/kpi/atlantida/properties/TelegramProperties.kt
+++ src/main/kotlin/ua/kpi/atlantida/properties/TelegramProperties.kt
@@ -3,9 +3,9 @@ package ua.kpi.atlantida.properties
 class TelegramProperties : MyProperties(FILE_NAME) {
 
     private companion object {
-        private val FILE_NAME = "telegram.keys"
-        private val PROP_TOKEN = "token"
-        private val PROP_USERNAME = "username"
+        const val FILE_NAME = "telegram.keys"
+        const val PROP_TOKEN = "token"
+        const val PROP_USERNAME = "username"
     }
 
     val token = readProperties(PROP_TOKEN)
